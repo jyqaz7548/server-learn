03781575989ab85c241519c590dc0d9200f4f696
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-06-27 20:00:42 +0900

    summer

--- crud/app.js
+++ crud/app.js
@@ -51,3 +51,26 @@ app.post("/webtoon", (req, res) => {
 app.listen(PORT, () => {
   console.log(`${PORT}에서 서버가 켜졌습니다.`);
 });
+
+app.delete("/webtoon/:id", (req, res) => {
+  const itemId = parseInt(req.params.id, 10);
+  const itemIndex = webtoon.findIndex((i) => i.id === itemId);
+  if (itemIndex !== -1) {
+    webtoon.splice(itemId + 1, 1);
+    res.status(204).send("데이터 삭제되었습니다.");
+  } else {
+    res.status(404).json({ mesage: "삭제할 데이터가 없습니다.;" });
+  }
+});
+
+app.put("/webtoon/:id", (req, res) => {
+  const itemId = parseInt(req.params.id, 10);
+  const itemIndex = webtoon.findIndex((i) => i.id === itemId);
+  if (itemIndex !== -1) {
+    const updatedItem = { ...webtoon[itemIndex + 1], ...req.body };
+    webtoon[itemIndex + 1 == updatedItem];
+    res.json(updatedItem);
+  } else {
+    res.status(404).json({ mesage: "업데이트할 데이터가 없습니다.;" });
+  }
+});
